74cbef55f0f7073d33fd6f0d804d1751560d7954
Zacharie Tevaearai <zacharie.tevaearai@epfl.ch>
2023-10-19 09:05:44 +0200

    Add simple stress test

---
+++ examples/stress_test.rs
@@ -0,0 +1,46 @@
+use std::time::Duration;
+
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}};
+use corentin::{prelude::*, coro_param::on_change};
+
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, CorentinPlugin))
+        .add_plugins(LogDiagnosticsPlugin::default())
+        .add_plugins(FrameTimeDiagnosticsPlugin)
+        .add_systems(Startup, setup_scene)
+        .run();
+}
+
+#[derive(Component)]
+struct Example(u32);
+
+fn setup_scene(
+    mut commands: Commands,
+) {
+
+    for _ in 0..10000 {
+        let tracker: ChangeTracker<Example> = ChangeTracker::new();
+        commands.spawn((Example(0), tracker))
+                .add(coroutine(
+                |mut s: Scope| async move {
+                    s.start_local(|mut s: Scope, mut ex: Wr<Example>| async move {
+                        loop {
+                            s.next_tick().await;
+                            ex.get_mut(&mut s).0 += 1;
+                        }
+                    });
+
+                    loop {
+                        let c1 = s.start(wait_on_example);
+                        let c2 = s.start(wait_on_example);
+                        s.all((c1, c2)).await;
+                    }
+                }
+            ));
+    }
+}
+
+async fn wait_on_example(mut s: Scope, on_change: OnChange<Example>) {
+    on_change.observe(&mut s).await;
+}
